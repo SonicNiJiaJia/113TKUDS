e4383c2f255eaadcbbf4b73e390e9254e5d8be81
SonicNiJiaJia <151108426+SonicNiJiaJia@users.noreply.github.com>
2025-07-02 09:51:17 +0800

    0702

---
+++ 0702/loopb.java
@@ -0,0 +1,17 @@
+public class loopb {
+    public static void main(String[] args) {
+        
+        int x = 0;
+        int count = 0;
+        for (int i = 2; i <= 100; i++ ) 
+        {
+            x =x+1;
+            count++;            
+        }
+        System.out.println(count);
+    }
+    
+}
+
+    
+
